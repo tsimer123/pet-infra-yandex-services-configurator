046361994b3bb83839621562782d4686eeeb5e27
Roman Martsev <matrixur@gmail.com>
2023-04-22 14:58:02 +0000

    feat: change setupping env

--- hacks/setup-env-yandex.sh
+++ hacks/setup-env-yandex.sh
@@ -2,19 +2,20 @@
 
 cloud_id=b1g4vva7tlne0vi7o5v2
 folder_id=b1gc4icvp3odr1k2i4pi
+terraform_state_keys_secret_id=e6qta9bg4j4qgc55t693
 
 yc init --cloud-id=${cloud_id} --folder-id=${folder_id}
 
-yc kms symmetric-crypto decrypt --id abjtocvapv6bm7q23lk6 --ciphertext-file ./hacks/cipher-secret-key --plaintext-file ./yc-secret-key
-yc kms symmetric-crypto decrypt --id abjtocvapv6bm7q23lk6 --ciphertext-file ./hacks/cipher-access-key --plaintext-file ./yc-access-key
+IAM_TOKEN=$(yc iam create-token)
 
 cat <<EOF >>./.env
-YC_STORAGE_ACCESS_KEY=$(cat ./yc-access-key)
-YC_STORAGE_SECRET_KEY=$(cat ./yc-secret-key)
+YC_STORAGE_ACCESS_KEY=$(curl -X GET -s \
+    -H "Authorization: Bearer ${IAM_TOKEN}" \
+    https://payload.lockbox.api.cloud.yandex.net/lockbox/v1/secrets/${terraform_state_keys_secret_id}/payload | jq -r .entries[0].textValue)
+YC_STORAGE_SECRET_KEY=$(curl -X GET -s \
+        -H "Authorization: Bearer ${IAM_TOKEN}" \
+        https://payload.lockbox.api.cloud.yandex.net/lockbox/v1/secrets/${terraform_state_keys_secret_id}/payload | jq -r .entries[1].textValue)
 YC_CLOUD_ID=${cloud_id}
 YC_FOLDER_ID=${folder_id}
-YC_TOKEN=$(yc iam create-token)
+YC_TOKEN=${IAM_TOKEN}
 EOF
-
-rm -f ./yc-secret-key
-rm -f ./yc-access-key
