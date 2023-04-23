4d725cf01e919f8584632f8bb59386de162661b9
Roman Martsev <matrixur@gmail.com>
2023-04-23 16:59:07 +0000

    feat(network): add kube network

--- cloud/network/subnet.tf
+++ cloud/network/subnet.tf
@@ -5,3 +5,12 @@ resource "yandex_vpc_subnet" "a_compute" {
   network_id     = yandex_vpc_network.this.id
   v4_cidr_blocks = ["10.5.0.0/24"]
 }
+
+resource "yandex_vpc_subnet" "kube" {
+  name           = "${local.zone}-kube"
+  folder_id      = yandex_resourcemanager_folder.this.id
+  zone           = local.zone
+  network_id     = yandex_vpc_network.this.id
+  v4_cidr_blocks = ["10.6.0.0/16"]
+}
+
